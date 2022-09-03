36ce045afbff696e23ad20142621748d3470eb76
Gafhito <gonzalo.ferrari@outlook.es>
2022-09-03 18:18:54 -0300

    Que son los logs y como usarlos

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,8 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 
+	private final Log LOGGER = LogFactory.getLog(FundamentosApplication.class);
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
@@ -37,5 +40,12 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBeanWithDependency.printWithDependency();
 		System.out.println(myBeanWithProperties.function());
 		System.out.println(userPojo.getEmail() + " - " + userPojo.getPassword());
+		try {
+			// error
+			int value = 10/0;
+			LOGGER.debug("Mi valor es: " + value);
+		} catch (Exception e) {
+			LOGGER.error("Esto es un error al dividir por cero " + e.getMessage());
+		}
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanDependencyImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBeanDependencyImplement.java
@@ -1,6 +1,11 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBeanDependencyImplement implements MyBeanWithDependency {
+
+    Log LOGGER = LogFactory.getLog(MyBeanDependencyImplement.class);
     private MyOperation myOperation;
 
     public MyBeanDependencyImplement(MyOperation myOperation) {
@@ -9,7 +14,9 @@ public class MyBeanDependencyImplement implements MyBeanWithDependency {
 
     @Override
     public void printWithDependency() {
+        LOGGER.info("Hemos ingresado al método printWithDependency");
         int numero = 1;
+        LOGGER.debug("El número enviado como parámetro a la dependencia opearación es: " + numero);
         System.out.println(myOperation.sum(numero));
         System.out.println("Hola desde la implementación de un bean con dependencia");
     }
