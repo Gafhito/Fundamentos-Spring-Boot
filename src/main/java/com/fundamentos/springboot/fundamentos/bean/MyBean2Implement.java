c5b05f411b1b6ec53cbf111aeb18c77ba7dc32b6
Gafhito <gonzalo.ferrari@outlook.es>
2022-09-03 20:33:35 -0300

    Modelado de entidades con JPA

--- src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBean2Implement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBean2Implement.java
@@ -1,8 +1,13 @@
 package com.fundamentos.springboot.fundamentos.bean;
 
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
 public class MyBean2Implement implements MyBean {
+    Log LOGGER = LogFactory.getLog(MyBean2Implement.class);
     @Override
     public void print() {
+        LOGGER.info("Implementación propia del bean número 2");
         System.out.println("Hola desde mi implementación 2 propia del bean 2");
     }
 }
