d4e107c7f4157f1490d3afeadf94b54d897a87a3
Gafhito <gonzalo.ferrari@outlook.es>
2022-09-04 18:16:30 -0300

    Uso de anotación transactional

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -7,6 +7,7 @@ import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
 import com.fundamentos.springboot.fundamentos.entity.User;
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import com.fundamentos.springboot.fundamentos.repository.UserRepository;
+import com.fundamentos.springboot.fundamentos.service.UserService;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -29,14 +30,16 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBeanWithProperties myBeanWithProperties;
 	private UserPojo userPojo;
 	private UserRepository userRepository;
+	private UserService usersService;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository, UserService usersService) {
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myBeanWithProperties = myBeanWithProperties;
 		this.userPojo = userPojo;
 		this.userRepository = userRepository;
+		this.usersService = usersService;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -45,12 +48,26 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//ejemplosAnteriores();
+		//saveUsersInDataBase();
+		//getInformationJpqlFromUser();
+
 		saveUsersInDataBase();
-		getInformationJpqlFromUser();
+	}
+
+	private void saveWithErrorTransactional() {
+		User test1 = new User("Test Transactional 1", "TestTransactional_1@domain.com", LocalDate.now());
+		User test2 = new User("Test Transactional 2", "TestTransactional_2@domain.com", LocalDate.now());
+		User test3 = new User("Test Transactional 3", "TestTransactional_3@domain.com", LocalDate.now());
+		User test4 = new User("Test Transactional 4", "TestTransactional_4@domain.com", LocalDate.now());
+
+		List<User> users = Arrays.asList(test1, test2, test3, test4);
+
+		usersService.saveTransactional(users);
+		usersService.getAllUsers().stream().forEach(user -> LOGGER.info("Este es el usuario dentro del método transaccional " + user));
 	}
 
 	private void getInformationJpqlFromUser() {
-		/*
+
 		LOGGER.info("Usuario con el método findByEmail: " +
 				userRepository.findByEmail("julie@mail.com")
 				.orElseThrow(() -> new RuntimeException("No se encontró el usuario")));
@@ -89,7 +106,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		userRepository.findByNameContainingOrderByIdDesc("user")
 				.stream()
 				.forEach(user -> LOGGER.info("Usuario encontrado con containing y ordenado: " + user));
-		 */
 
 		LOGGER.info("El usuario a partir del named parameter es: " + userRepository.getAllByBirthDateAndEmail(LocalDate.of(2021,5,2), "daniela@mail.com")
 				.orElseThrow(() -> new RuntimeException("No se encontró el usuario a partir del named parameter")));
